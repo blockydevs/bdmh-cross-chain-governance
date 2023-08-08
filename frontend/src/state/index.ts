802eacddb65a55c50024bfc9d1c0c3358146068a
Michal Rozek <michalrozek90@gmail.com>
2023-08-08 16:31:54 +0200

    fix: chain id error

--- frontend/src/state/index.ts
+++ frontend/src/state/index.ts
@@ -6,7 +6,6 @@ import { isTestEnv } from 'utils/env'
 import { updateVersion } from './global/actions'
 import { sentryEnhancer } from './logging'
 import reducer from './reducer'
-import { routingApi } from './routing/slice'
 
 const PERSISTED_KEYS: string[] = ['user', 'transactions', 'lists']
 
@@ -20,11 +19,8 @@ const store = configureStore({
         // meta.arg and meta.baseQueryMeta are defaults. payload.trade is a nonserializable return value, but that's ok
         // because we are not adding it into any persisted store that requires serialization (e.g. localStorage)
         ignoredActionPaths: ['meta.arg', 'meta.baseQueryMeta', 'payload.trade'],
-        ignoredPaths: [routingApi.reducerPath],
       },
-    })
-      .concat(routingApi.middleware)
-      .concat(save({ states: PERSISTED_KEYS, debounce: 1000 })),
+    }).concat(save({ states: PERSISTED_KEYS, debounce: 1000 })),
   preloadedState: load({ states: PERSISTED_KEYS, disableWarnings: isTestEnv() }),
 })
 
