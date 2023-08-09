76ed0b9d3a9b71c1e6c259f5e8db9a8261c0f2ef
Tomasz Warczykowski <tomasz.warczykowski@silksh.pl>
2023-08-09 16:56:23 +0200

    CI

--- smartcontracts/deploy.sh
+++ smartcontracts/deploy.sh
@@ -3,13 +3,13 @@
 echo "Vote token deployment"
 forge script script/VHMTDeployment.s.sol:VHMTDeployment --rpc-url $SEPOLIA_RPC_URL --broadcast --verify
 
-# hub vote deployment
+# hub contract deployment
 echo "Deploying Hub contract"
 forge script script/HubDeployment.s.sol:HubDeployment --rpc-url $SEPOLIA_RPC_URL --broadcast --verify
 GOVERNOR_ADDRESS="$(cat "broadcast/HubDeployment.s.sol/$CHAIN/run-latest.json" | jq -r '.transactions[0].contractAddress')"
 TIMELOCK_ADDRESS="$(cat "broadcast/HubDeployment.s.sol/$CHAIN/run-latest.json" | jq -r '.transactions[0].contractAddress')"
 
-# spoke vote deployment
+# spoke contract deployment
 echo "Deploy Spoke contract"
 forge script script/SpokeDeployment.s.sol:SpokeDeployment --rpc-url $POLYGON_MUMBAI_RPC_URL --etherscan-api-key $MUMBAI_ETHERSCAN_API_KEY --broadcast --legacy --verify
 SPOKE_ADDRESSES="$(cat "broadcast/SpokeDeployment.s.sol/$CHAIN/run-latest.json" | jq -r '.receipts[].logs | map(.address) | join(",")')"
