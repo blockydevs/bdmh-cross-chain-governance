42039a477e2f730a1edccd5211a8272f6daae05a
Bartosz Solka <bartosz.solka@blockydevs.com>
2023-08-30 12:13:27 +0200

    bug fixing; automatic relayer related changes

--- smartcontracts/deploy.sh
+++ smartcontracts/deploy.sh
@@ -15,16 +15,15 @@ echo "Deploy Spoke contracts"
 COUNT=$(echo "$SPOKE_PARAMS" | jq -s '. | length')
 for ((i=0; i < $COUNT; i++)); do
   # set vars
-  SPOKE_CORE_BRIDGE_ADDRESS=$(echo "$SPOKE_PARAMS" | jq -r -s --argjson i "$i" '.[$i].SPOKE_CORE_BRIDGE_ADDRESS')
+  SPOKE_AUTOMATIC_RELAYER_ADDRESS=$(echo "$SPOKE_PARAMS" | jq -r -s --argjson i "$i" '.[$i].SPOKE_AUTOMATIC_RELAYER_ADDRESS')
   SPOKE_CHAIN_ID=$(echo "$SPOKE_PARAMS" | jq -r -s --argjson i "$i" '.[$i].SPOKE_CHAIN_ID')
   SPOKE_RPC_URL=$(echo "$SPOKE_PARAMS" | jq -r -s --argjson i "$i" '.[$i].SPOKE_RPC_URL')
   SPOKE_ETHERSCAN_API_KEY=$(echo "$SPOKE_PARAMS" | jq -r -s --argjson i "$i" '.[$i].SPOKE_ETHERSCAN_API_KEY')
   SPOKE_HM_TOKEN_ADDRESS=$(echo "$SPOKE_PARAMS" | jq -r -s --argjson i "$i" '.[$i].SPOKE_HM_TOKEN_ADDRESS')
 
-  HM_TOKEN_ADDRESS=$HUB_HM_TOKEN_ADDRESS
+  HM_TOKEN_ADDRESS=$SPOKE_HM_TOKEN_ADDRESS
   forge script script/VHMTDeployment.s.sol:VHMTDeployment --rpc-url $SPOKE_RPC_URL --etherscan-api-key $SPOKE_ETHERSCAN_API_KEY --broadcast --verify
-  VOTE_TOKEN_ADDRESS="$(cat "broadcast/VHMTDeployment.s.sol/$SPOKE_CHAIN_ID/run-latest.json" | jq -r '.transactions[0].contractAddress')"
-  SPOKE_VOTE_TOKEN_ADDRESS=$VOTE_TOKEN_ADDRESS
+  SPOKE_VOTE_TOKEN_ADDRESS="$(cat "broadcast/VHMTDeployment.s.sol/$SPOKE_CHAIN_ID/run-latest.json" | jq -r '.transactions[0].contractAddress')"
 
   forge script script/SpokeDeployment.s.sol:SpokeDeployment --rpc-url $SPOKE_RPC_URL --etherscan-api-key $SPOKE_ETHERSCAN_API_KEY --broadcast --legacy --verify
   SPOKE_ADDRESS="$(cat "broadcast/SpokeDeployment.s.sol/$SPOKE_CHAIN_ID/run-latest.json" | jq -r '.transactions[0].contractAddress')"
