00366c155d2b88ec86c66112420ad6cab8b66bc7
Bartosz Solka <bartosz.solka@blockydevs.com>
2023-08-31 22:39:50 +0200

    typo fix

--- smartcontracts/deploy.sh
+++ smartcontracts/deploy.sh
@@ -32,7 +32,7 @@ for ((i=0; i <= $COUNT; i++)); do
   export SPOKE_ADDRESS="$(cat "broadcast/SpokeDeployment.s.sol/$SPOKE_CHAIN_ID/run-latest.json" | jq -r '.transactions[0].contractAddress')"
   export SPOKE_WORMHOLE_CHAIN_IDS="${SPOKE_WORMHOLE_CHAIN_IDS},${SPOKE_WORMHOLE_CHAIN_ID}"
   export SPOKE_ADDRESSES="${SPOKE_ADDRESSES},${SPOKE_ADDRESS}"
-  DEPLOYED_SPOKES="${DEPLOYED_SPOKES}Spoke $i address: $SPOKE_ADDRESS\nSpoke $i wormholeChainId: $SPOKE_WORMHOLE_CHAIN_ID\n\n'
+  DEPLOYED_SPOKES="${DEPLOYED_SPOKES}Spoke $i address: $SPOKE_ADDRESS\nSpoke $i wormholeChainId: $SPOKE_WORMHOLE_CHAIN_ID\n\n"
 done
 # remove first character
 export SPOKE_WORMHOLE_CHAIN_IDS=${SPOKE_WORMHOLE_CHAIN_IDS:1}
