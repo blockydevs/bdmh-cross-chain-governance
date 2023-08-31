8d73cd7ea600749888f7b196387d2ee0caf373c4
Bartosz Solka <bartosz.solka@blockydevs.com>
2023-08-31 09:54:28 +0200

    fixes to deployment script

--- smartcontracts/deploy.sh
+++ smartcontracts/deploy.sh
@@ -8,19 +8,19 @@ VOTE_TOKEN_ADDRESS="$(cat "broadcast/VHMTDeployment.s.sol/$HUB_CHAIN_ID/run-late
 # hub contract deployment
 echo "Deploying Hub contract"
 forge script script/HubDeployment.s.sol:HubDeployment --rpc-url $HUB_RPC_URL --etherscan-api-key $HUB_ETHERSCAN_API_KEY --broadcast --verify
-GOVERNOR_ADDRESS="$(cat "broadcast/HubDeployment.s.sol/$HUB_CHAIN_ID/run-latest.json" | jq -r '.transactions[0].contractAddress')"
 TIMELOCK_ADDRESS="$(cat "broadcast/HubDeployment.s.sol/$HUB_CHAIN_ID/run-latest.json" | jq -r '.transactions[0].contractAddress')"
+GOVERNOR_ADDRESS="$(cat "broadcast/HubDeployment.s.sol/$HUB_CHAIN_ID/run-latest.json" | jq -r '.transactions[1].contractAddress')"
 
 # spoke contract deployment
 echo "Deploy Spoke contracts"
 COUNT=$(echo "$SPOKE_PARAMS" | jq -s '. | length')
 for ((i=0; i < $COUNT; i++)); do
   # set vars
-  SPOKE_AUTOMATIC_RELAYER_ADDRESS=$(echo "$SPOKE_PARAMS" | jq -r -s --argjson i "$i" '.[$i].SPOKE_AUTOMATIC_RELAYER_ADDRESS')
-  SPOKE_CHAIN_ID=$(echo "$SPOKE_PARAMS" | jq -r -s --argjson i "$i" '.[$i].SPOKE_CHAIN_ID')
-  SPOKE_RPC_URL=$(echo "$SPOKE_PARAMS" | jq -r -s --argjson i "$i" '.[$i].SPOKE_RPC_URL')
-  SPOKE_ETHERSCAN_API_KEY=$(echo "$SPOKE_PARAMS" | jq -r -s --argjson i "$i" '.[$i].SPOKE_ETHERSCAN_API_KEY')
-  SPOKE_HM_TOKEN_ADDRESS=$(echo "$SPOKE_PARAMS" | jq -r -s --argjson i "$i" '.[$i].SPOKE_HM_TOKEN_ADDRESS')
+  SPOKE_AUTOMATIC_RELAYER_ADDRESS=$(echo "$SPOKE_PARAMS" | jq -r --argjson i "$i" '.[$i].SPOKE_AUTOMATIC_RELAYER_ADDRESS')
+  SPOKE_CHAIN_ID=$(echo "$SPOKE_PARAMS" | jq -r --argjson i "$i" '.[$i].SPOKE_CHAIN_ID')
+  SPOKE_RPC_URL=$(echo "$SPOKE_PARAMS" | jq -r --argjson i "$i" '.[$i].SPOKE_RPC_URL')
+  SPOKE_ETHERSCAN_API_KEY=$(echo "$SPOKE_PARAMS" | jq -r --argjson i "$i" '.[$i].SPOKE_ETHERSCAN_API_KEY')
+  SPOKE_HM_TOKEN_ADDRESS=$(echo "$SPOKE_PARAMS" | jq -r --argjson i "$i" '.[$i].SPOKE_HM_TOKEN_ADDRESS')
 
   HM_TOKEN_ADDRESS=$SPOKE_HM_TOKEN_ADDRESS
   forge script script/VHMTDeployment.s.sol:VHMTDeployment --rpc-url $SPOKE_RPC_URL --etherscan-api-key $SPOKE_ETHERSCAN_API_KEY --broadcast --verify
