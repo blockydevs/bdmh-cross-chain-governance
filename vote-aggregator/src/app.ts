d972fc8339f24691af57734dc299272de2dc1129
MQWal <michal.walczak@blockydevs.com>
2023-09-01 14:23:01 +0200

    Added cors

--- vote-aggregator/src/app.ts
+++ vote-aggregator/src/app.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import proposalRoutes from './routes/proposalRoutes';
 
 const app = express();
 const port = process.env.NODE_PORT || 8080;
+
+app.use(cors());
 
 app.use(proposalRoutes);
 
