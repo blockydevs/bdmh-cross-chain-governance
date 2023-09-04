4b1d37b488447af24b75b1b706eb0c94d5ad0482
Michal Rozek <michalrozek90@gmail.com>
2023-09-04 12:20:31 +0200

    feat: aggregateVotes logic

--- frontend/src/utils/aggregateVotes.ts
+++ frontend/src/utils/aggregateVotes.ts
@@ -1,12 +1,37 @@
+import { BigNumber } from '@ethersproject/bignumber'
 import { ChainVoteData } from 'api/votes'
 
+const toBigNumber = (value: number | string): BigNumber => {
+  return BigNumber.from(value.toString())
+}
+
+const truncateLargeNumber = (value: BigNumber): number => {
+  const divisor = BigNumber.from('1000000000000000000')
+  const quotient = value.div(divisor)
+  const remainder = value.mod(divisor)
+  const decimalPart = remainder.mul(BigNumber.from('1000')).div(divisor).toNumber() / 1000
+  const rounded = Math.round((quotient.toNumber() + decimalPart) * 100) / 100
+
+  return rounded
+}
+
 export const aggregateVotes = (data: ChainVoteData[]) => {
-  return data.reduce(
+  const aggregatedBigNumbers = data.reduce(
     (acc, chainData) => ({
-      for: acc.for + chainData.for,
-      against: acc.against + chainData.against,
-      abstain: acc.abstain + chainData.abstain,
+      for: acc.for.add(toBigNumber(chainData.for)),
+      against: acc.against.add(toBigNumber(chainData.against)),
+      abstain: acc.abstain.add(toBigNumber(chainData.abstain)),
     }),
-    { for: 0, against: 0, abstain: 0 }
+    {
+      for: BigNumber.from(0),
+      against: BigNumber.from(0),
+      abstain: BigNumber.from(0),
+    }
   )
+
+  return {
+    for: truncateLargeNumber(aggregatedBigNumbers.for),
+    against: truncateLargeNumber(aggregatedBigNumbers.against),
+    abstain: truncateLargeNumber(aggregatedBigNumbers.abstain),
+  }
 }
