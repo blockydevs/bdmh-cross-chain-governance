66619c61c848f49a3f3ac43a33cf5704612808b5
MQWal <michal.walczak@blockydevs.com>
2023-09-05 13:13:27 +0200

    runapp to cors origin added

--- vote-aggregator/src/config/cors.ts
+++ vote-aggregator/src/config/cors.ts
@@ -1,5 +1,5 @@
 import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
-    origin: ['http://localhost:3000', 'https://blockydevs.github.io']
+    origin: ['http://localhost:3000', 'https://blockydevs.github.io', 'https://vote-aggregator-scsonez34a-ew.a.run.app']
 };
