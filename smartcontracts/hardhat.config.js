4378d6519eb48c56d52e509b6551777c494e4af8
MQWal <michal.walczak@blockydevs.com>
2023-09-06 14:33:08 +0200

    hardhat config added

---
+++ smartcontracts/hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-foundry")
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true
+    }
+  },
+};
