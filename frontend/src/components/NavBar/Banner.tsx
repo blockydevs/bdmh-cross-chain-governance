55561bf105c5f3c70ac4043cfb772bc648c96cd3
Michal Rozek <michalrozek90@gmail.com>
2023-10-11 09:54:31 +0200

    feat: banner with label implemented

--- frontend/src/components/NavBar/Banner.tsx
+++ frontend/src/components/NavBar/Banner.tsx
@@ -9,13 +9,22 @@ const BannerWrapper = styled.div`
   align-items: center;
   padding: 12px 60px;
   z-index: 2;
-  background-color: yellow;
+  background-color: ${({ theme }) => theme.banner};
+
+  > div {
+    font-size: 24px;
+    color: white;
+
+    @media only screen and (max-width: ${({ theme }) => `${theme.breakpoint.sm}px`}) {
+      font-size: 16px;
+    }
+  }
 `
 
 const Banner = () => {
   return (
     <BannerWrapper>
-      <ThemedText.BodyPrimary>This is a testing version</ThemedText.BodyPrimary>
+      <ThemedText.BodySecondary>This is a testing version</ThemedText.BodySecondary>
     </BannerWrapper>
   )
 }
