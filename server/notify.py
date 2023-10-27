2c6b6497cec1e45c021b8f25acb874b0bc82312b
AbubakrChan <89600478+AbubakrChan@users.noreply.github.com>
2023-10-27 16:17:54 +0500

    Added discord notification code.
    Added discord notification code which is executed once the createproposal function is executed.

--- server/notify.py
+++ server/notify.py
@@ -1,30 +1,48 @@
 from web3 import Web3
+import discord
+import asyncio
+
+# GLOBALS
+# for web3 integration
+ETHEREUM_NODE_URL = 'YOUR_ETHEREUM_NODE_URL' 
+CONTRACT_ADDRESS = 'YOUR_CONTRACT_ADDRESS'
+CONTRACT_ABI = []
+#for discord integration
+CHANNEL_ID = 1165138239301107803  #Enter your discord channel id
+BOT_TOKEN = 'MTE2NjUzNjU2OTk5ODU0MDg5MA.G5vRdW.8JoOWg-ypzVyw3GkpILJnRgeEDUsbSIyDImWec'
+
+NOTIFICATION = 'Hello, Mehidy I wanted to inform you that someone has created a proposal.'
+# NOTIFICATION = '.'
+
+should_notify = True
 
 
-ethereum_node_url = ""  # 'YOUR_ETHEREUM_NODE_URL'
+# Discord bot setup
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
 
-if ethereum_node_url:
-    w3 = Web3(Web3.HTTPProvider(ethereum_node_url))
-else:
-    w3 = None
+# if ETHEREUM_NODE_URL:
+#     w3 = Web3(Web3.HTTPProvider(ETHEREUM_NODE_URL))
+# else:
+#     w3 = None
+    
+w3 = Web3(Web3.HTTPProvider(ETHEREUM_NODE_URL)) if ETHEREUM_NODE_URL else None    
 
-
-run_script = False
 
 if w3:
-    contract_address = ""
-    
-    contract_abi = []
-
     def event_callback(event):
-        global run_script
         print("Event received")
-        run_script = True
+        if client.is_ready():
+            channel = client.get_channel(CHANNEL_ID)
+            asyncio.ensure_future(channel.send(NOTIFICATION))
 
-    # Start listening to the event (if contract address and ABI are provided)
-    if contract_address and contract_abi:
-        contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+    if CONTRACT_ADDRESS and CONTRACT_ABI:
+        contract = w3.eth.contract(address=CONTRACT_ADDRESS, abi=CONTRACT_ABI)
         event_filter = contract.events.RunFunctionExecuted.createFilter(
             fromBlock="latest"
         )
@@ -32,5 +50,18 @@ if w3:
             for event in event_filter.get_new_entries():
                 event_callback(event)
 
-if run_script:
-    print("Code to be executed when the CreateProposal function is ran.")
+
+# this is just for testing the notifications without events can be removed once the project is done.
+if should_notify == True:
+    @client.event
+    async def on_ready():
+        print(f'We have logged in as {client.user}')
+        if client.is_ready():
+            channel = client.get_channel(CHANNEL_ID)
+            if channel:
+                asyncio.ensure_future(channel.send(NOTIFICATION))
+        else:
+            print("Error: Couldn't find the specified channel.")
+
+
+client.run(BOT_TOKEN)
